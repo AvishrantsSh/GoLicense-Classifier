5cba5c1eee7198850cc00693a0e35a86fcf53d1b
Avishrantsh <avishrants@gmail.com>
2021-05-30 12:28:02 +0530

    Version Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="testpkg_avishrantssh",
-    version="0.0.3",
+    version="0.0.4",
     author="AvishrantSh (Avishrant Sharma)",
     author_email="<avishrants@gmail.com>",
     description="A Python based License Classifier",
