17a4c8c5ddcb86ddeb3d839bf2283b282a9ffc20
Avishrantsh <avishrants@gmail.com>
2021-06-05 20:21:36 +0530

    Syntax Updates

--- src/LicenseClassifier/classifier.py
+++ src/LicenseClassifier/classifier.py
@@ -1,8 +1,8 @@
 import ctypes
-from LicenseClassifier.error import *
-from os.path import join, dirname, isfile, isdir, exists
-from os import walk, listdir
-from time import time
+from os import listdir, walk
+from os.path import dirname, exists, isdir, isfile, join
+
+from LicenseClassifier.error import PathIsDir, PathIsFile
 
 
 class LicenseClassifier:
@@ -33,7 +33,11 @@ class LicenseClassifier:
         if isdir(filepath):
             raise PathIsDir
 
-        res = self._match(LicenseClassifier._ROOT.encode("utf-8"), filepath.encode("utf-8"), output.encode('utf-8'))
+        res = self._match(
+            LicenseClassifier._ROOT.encode("utf-8"),
+            filepath.encode("utf-8"),
+            output.encode("utf-8"),
+        )
         return res.decode("utf-8")
 
     def catalogueDir(self, root, searchSubDir=True, output="result.json"):
@@ -41,18 +45,24 @@ class LicenseClassifier:
         if not exists(root):
             raise FileNotFoundError
 
-        filepath = list()
         if isfile(root):
             raise PathIsFile
 
         if searchSubDir:
-            for (dirpath, _, filenames) in walk(root):
-                filepath += [join(dirpath, f) for f in filenames]
+            filepath = [
+                join(dirpath, f)
+                for (dirpath, _, filenames) in walk(root)
+                for f in filenames
+            ]
         else:
             filepath = [join(root, f) for f in listdir(root) if isfile(join(root, f))]
 
         filepath = "\n".join(filepath)
-        res = self._match(LicenseClassifier._ROOT.encode("utf-8"), filepath.encode("utf-8"), output.encode('utf-8'))
+        res = self._match(
+            LicenseClassifier._ROOT.encode("utf-8"),
+            filepath.encode("utf-8"),
+            output.encode("utf-8"),
+        )
         return res.decode("utf-8")
 
     def loadCustom(self, libpath):
